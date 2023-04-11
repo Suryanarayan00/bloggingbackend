fea0dcf0f1195f623de52badd26b98f64e2f3cda
root <root@vps.server.local>
2023-04-11 16:15:23 +0000

    Demployment commit

--- blogPost/urls.py
+++ blogPost/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apis.urls')),
     path('', include('website.ulrs'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
